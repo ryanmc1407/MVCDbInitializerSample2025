a100a53222548496ea7f9323b002073bac792914
ryanmc1407 <ryanmcclelland@msn.com>
2026-02-22 14:59:16 +0000

    S00236888 Project setup

---
+++ Week1Lab12026/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Week1Lab12026.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
